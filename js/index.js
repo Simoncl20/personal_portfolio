ec0b6c0bf662cf436a7bcbfeb56c5a95cfe22224
Simoncl20 <simoncalderonl2010@gmail.com>
2023-11-17 02:31:22 -0500

    Swiper HTML and JS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/*==================== Sticky Navbar =================== */
+
 function stickyHeader() {
     window.onscroll = () => {
         let header = document.querySelector("header");
@@ -6,10 +8,30 @@ function stickyHeader() {
     };
 }
 
+/*==================== Swiper =================== */
+
+function swiper() {
+    let swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+}
+
+
 
 
 function main() {
     stickyHeader();
+    swiper();
 
 }
 
